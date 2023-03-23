9cc7157ece3806ed96bda9248c8a7135bd863edb
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2023-03-23 20:55:21 -0300

    Update main.cpp

--- Conta-Bancaria/main.cpp
+++ Conta-Bancaria/main.cpp
@@ -1,10 +1,20 @@
+//Programa introduzindo o uso de Struct por meio de uma conta bancária com capacidade de iniciação de conta, saque, depósito e extrato.
+
 #include <iostream> 
 #include "conta.h"
 
 using namespace std;
+
 int main(){
-	conta c;
-	c = iniciar(202, 1111);
-	extrato(c);
+	
+	//Definindo a variável novaConta com o tipo 'conta'
+	conta novaConta;
+	
+	//Realizando as Operações
+	novaConta = iniciar(2023, 100);
+	deposito(novaconta, 1500);
+	saque(novaConta, 120);
+	extrato(novaConta);
+
 	return 0;
 }
